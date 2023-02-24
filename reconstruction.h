fc2763fd4f850c957580359d41dfaba5daac50d3
Todd Tavernier <79160239+ToddTavernier@users.noreply.github.com>
2023-02-24 15:36:59 +0100

    Add files via upload

---
+++ reconstruction.h
@@ -0,0 +1,16 @@
+# ifndef RECONSTRUCTION_H
+# define RECONSTRUCTION_H
+
+#include <err.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "pixel_operations.h"
+#include "display.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int reconstruction(int chiffres,char* filename);
+
+
+#endif
